36637c719c2f47ffeeb87d70f52dd81d6b538c13
Johan Bichel <public@johan.cc>
2023-11-21 15:05:59 +0100

    Chore: use github reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	testMatch: '**/*e2e.spec.ts',
+	reporter: process.env.CI ? 'github' : 'list',
 	webServer: {
 		command: 'pnpm run preview',
 		port: 4173
